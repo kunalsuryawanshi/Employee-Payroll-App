05e9efe052f20b5f37a09f463bbd9fbcb16166b4
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-10-25 23:50:40 +0530

    Added: Employee Payroll Data Object

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,6 +1,9 @@
+/*
+Event Listener on Salary Range to display appropriate value
+*/
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
-salary.addEventListener('input',function(){
+salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
