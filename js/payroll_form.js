6b3a94ac2eb7675300721033242f41602c94225b
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-10-26 23:18:36 +0530

    Added: On Document Set Event Listener

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -8,3 +8,24 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+
+/*
+ * Day 44 LocalStorage
+ * UC2 – On Document Load Set Event Listeners
+ */
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = " ";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+});
